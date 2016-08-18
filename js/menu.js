e76252ffd70acd90f14b24a5ccdb8d3da7a16ea5
kmiloarguello <camilo.arguello.rincon@gmail.com>
2016-08-18 13:17:29 -0500

    Añadiendo archivos JS

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+// JS
+$(document).ready(main); // cuando cargue la pantalla
+
+var contador = 1;
+
+function main(){
+    $('.menu_bar').click(function(){
+        if(contador == 1){
+            $('.nav').animate({
+                left: '0'
+            });
+            contador = 0;
+        }else{
+            contador = 1;
+            $('.nav').animate({
+                left: '-100%'
+        });
+    }}); //eventos
+}
