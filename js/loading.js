7cefc2bb819dd35e0a02d437cb15170673986691
kmiloarguello <camilo.arguello.rincon@gmail.com>
2016-08-19 09:49:17 -0500

    Desarrollo loading y primera sección en responsive

--- js/loading.js
+++ js/loading.js
@@ -4,13 +4,13 @@
   });*/
 
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 20000);
+    var intervalID = window.setInterval(checkReady, 100);
     
     $('.count').each(function () {
         $(this).prop('Counter',0).animate({
         Counter: $(this).text()
         }, {
-        duration: 20000,
+        duration: 1000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
