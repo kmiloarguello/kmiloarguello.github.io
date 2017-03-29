eb192b72df5e792aeae5d0bdb841e0af40da8813
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-03-29 17:36:26 -0500

    adding portfolios

--- Engage_CERT/ENGAGE_CERT_PDF/js/local.js
+++
@@ -1,5 +0,0 @@
-// --- local.js --- //
-$( document ).ready(function() {
-    // (previous, next, first, last)
-	navCustomSwipe("CO_ULTIVA_UCI_2016_CO1.0_V1.0_003","","CO_ULTIVA_UCI_2016_CO1.0_V1.0_005","","CO_ULTIVA_UCI_2016_CO1.0_V1.0_001","","CO_ULTIVA_UCI_2016_CO1.0_V1.0_023","");
-});
